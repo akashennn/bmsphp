7b831988bbf2d35f0adf7f43a55c07cd6d5b5e66
Akash Perera <akashennn@gmail.com>
2018-11-10 11:15:44 +0530

    New: New UI for Search

--- application/views/books/result.php
+++ application/views/books/result.php
@@ -16,21 +16,45 @@ tr:nth-child(even) {
 }
 </style>
 
-<h2 style="text-align:center"> Search result for "<?= $keyword?>"</h2>
+<section class="jumbotron text-center">
+  <div class="container">
+    <h2 style="text-align:center"> Search result for "<?= $keyword?>"</h2>
+    <p class="lead text-muted">
+    “A reader lives a thousand lives before he dies, said Jojen. The man who never reads lives only one.” 
+    <br>
+    ― George R.R. Martin
+    </p>
+    <!-- <p>
+      <a href="#" class="btn btn-primary my-2">Main call to action</a>
+      <a href="#" class="btn btn-secondary my-2">Secondary action</a>
+    </p> -->
+  </div>
+</section>
+
+<div class="container" style="margin-bottom:80px;">
+<?php echo form_open('books/search'); ?>
+<div class="input-group mb-3">
+  <input type="text" class="form-control" name="keyword" placeholder="Search for a title or an author">
+  <div class="input-group-append">
+    <button class="btn btn-success" type="submit">Search</button> 
+  </div>
+</div>
+</form>
+</div>
 
 <div class="container">
-<table>
-  <tr>
-    <th>Title</th>
-    <th>Author</th>
-    <th>Price</th>
-  </tr>
-  <?php foreach ($cat as $cat) : ?>
-  <tr>
-    <td><?php echo $cat['title']; ?></td>
-    <td><?php echo $cat['author']; ?></td>
-    <td><?php echo $cat['price']; ?></td>
-  </tr>
-  <?php endforeach; ?>
-</table>
+<div class="row" style="margin: 0 50px">
+<?php foreach ($cat as $cat) : ?>
+<div class="card" style="width: 18rem; margin: 10px 20px">
+
+  <a href="<?php echo site_url('book/view/'.$cat['id']);?>">
+  <img class="card-img-top" style="height: 390px" src="<?php echo $cat['image_url']; ?>" alt="Card image cap">
+  </a>
+  <div class="card-body">
+    <h5 class="card-title text-center"><?php echo $cat['title']; ?></h5>
+  </div>
+
+</div>
+<?php endforeach; ?>
+</div>
 </div>
